d82f6af6df78c1cd1b2850109aaa748554c45084
hakosensei <hakosensei233@gmail.com>
2026-09-06 07:10:43 +0800

    Bump version to v0.8.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.taimaninviewer"
         minSdk = 23
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.8"
+        versionCode = 10
+        versionName = "0.8.1"
     }
 
     signingConfigs {
